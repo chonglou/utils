408f9479713a9a818a2ce231009590e10652aca0
flamen <jitang.zheng@gmail.com>
2014-05-30 12:38:26 -0700

    message和jobsender分离

--- lib/brahma/job.rb
+++ lib/brahma/job.rb
@@ -1,37 +1,55 @@
 module Brahma
-  class JobSender
-    def initialize(name, redis)
-      @redis = redis
-      @name = name
-    end
+  module Job
+    module Request
+      module_function
 
-    def echo(message)
-      send({type: :echo, message: message})
-    end
+      def echo(message)
+        {type: :echo, message: message}
+      end
 
-    def email(to, subject, content)
-      send({type: :email, to: to, subject: subject, content: content, create: Time.now, timeout: 60*60*24})
-    end
-
-    def send(request={})
-      @redis.with do |conn|
-        conn.lpush queue, Marshal.dump(request)
+      def email(to, subject, content)
+        {type: :email, to: to, subject: subject, content: content, create: Time.now, timeout: 60*60*24}
       end
     end
 
+    class RabbitJobSender
+      def initialize
 
-    def receive(timeout)
-      @redis.with do |conn|
-        task = conn.brpop(queue, timeout)
-        unless task.nil?
-          yield Marshal.load(task[1])
+      end
+      def send(request)
+
+      end
+      def receive(timeout)
+
+      end
+    end
+
+    class RedisSender
+      def initialize(name, redis)
+        @redis = redis
+        @name = name
+      end
+
+      def send(request)
+        @redis.with do |conn|
+          conn.lpush queue, Marshal.dump(request)
         end
       end
-    end
 
-    private
-    def queue
-      "worker://#{@name}"
+
+      def receive(timeout)
+        @redis.with do |conn|
+          task = conn.brpop(queue, timeout)
+          unless task.nil?
+            yield Marshal.load(task[1])
+          end
+        end
+      end
+
+      private
+      def queue
+        "worker://#{@name}"
+      end
     end
   end
 end
